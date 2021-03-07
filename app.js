80008454230d472368b21d0a82472964635bad11
Cricky <crickmonster@gmail.com>
2021-03-06 21:27:40 -0500

    code updates - following along with class zoom

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var stocksRouter = require('./routes/stocks');
+var walletRouter = require('./routes/wallet');
+
+
 var app = express();
+
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +28,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// write some code here to bootstrap your wallet with 100,000
+
+app.use('/api/v1/stocks', stocksRouter);
+
+app.use('/api/v1/wallet', walletRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
